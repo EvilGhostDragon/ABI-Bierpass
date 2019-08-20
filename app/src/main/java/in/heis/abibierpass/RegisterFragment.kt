adbe8eec5f737576a0f9744a911859aa89e5a6e0
EvilGhostDragon <simon@heis.in>
2019-08-21 00:25:07 +0200

    code - register form check

--- app/src/main/java/in/heis/abibierpass/RegisterFragment.kt
+++ app/src/main/java/in/heis/abibierpass/RegisterFragment.kt
@@ -1,11 +1,14 @@
 package `in`.heis.abibierpass
 
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.fragment_register.*
 
 
 class RegisterFragment : Fragment() {
@@ -20,5 +23,70 @@ class RegisterFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         activity!!.nav_view.menu.findItem(R.id.nav_acc_register).isChecked = true
+        btn_acc_register.setOnClickListener {
+            // Toast.makeText(context, editText_fname.text, Toast.LENGTH_SHORT).show()
+            btn_acc_register.isEnabled = false
+            if (isFormOk()) {
+                Toast.makeText(context, "ok", Toast.LENGTH_SHORT).show()
+
+            }
+            btn_acc_register.isEnabled = true
+        }
+    }
+
+    private fun isFormOk(): Boolean {
+        var state = true
+        var item = editText_fname
+        while (true) {
+            item.setTextColor(Color.BLACK)
+            if (item.text.contains(" ") or item.text.isEmpty()) {
+                if (item.text.contains(" ")) item.setTextColor(Color.RED)
+                state = false
+                item.error = "Felder dürfen nicht leer sein oder Leerzeichen beinhalten"
+            }
+            if ((item == editText_mail) and item.text.isNotEmpty()) {
+                if (!item.text.contains("@") or !item.text.contains(".")) {
+                    item.setTextColor(Color.RED)
+                    state = false
+                    item.error = "Gib eine gültige E-Mail Adresse an"
+                }
+            }
+            if ((item == editText_pswd) and (item.text.isNotEmpty())) {
+                if (item.text.count() < 6) {
+                    item.error = "Dein Passwort muss aus mindestens 6 Zeichen bestehen"
+                }
+            }
+            if ((item == editText_pswd2) and (item.text.isNotEmpty())) {
+                if (editText_pswd.text.toString() != item.text.toString()) {
+                    item.setTextColor(Color.RED)
+                    state = false
+                    item.error = "Überprüfe dein Passwort"
+                }
+            }
+            when (item) {
+                editText_fname -> {
+                    item = editText_lname
+
+                }
+                editText_lname -> {
+                    item = editText_vulgo
+                }
+                editText_vulgo -> {
+                    item = editText_mail
+                }
+                editText_mail -> {
+                    item = editText_pswd
+                }
+                editText_pswd -> {
+                    item = editText_pswd2
+                }
+                editText_pswd2 -> {
+                    return state
+                }
+            }
+
+            //if(editText_fname.text == null)Toast.makeText(context, editText_fname.text, Toast.LENGTH_SHORT).show()
+        }
+        //return state
     }
 }
