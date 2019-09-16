5fdecb03bc764f8b11b9e08e1d5c56f5bf0dca42
EvilGhostDragon <simon@heis.in>
2019-09-16 05:49:55 +0200

    code - in theory everything set up

--- app/src/main/java/in/heis/abibierpass/OrderBeerFragment.kt
+++ app/src/main/java/in/heis/abibierpass/OrderBeerFragment.kt
@@ -14,9 +14,7 @@ import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.fragment_order_beer.*
 import org.json.JSONObject
 
-/**
- * A simple [Fragment] subclass.
- */
+//TODO("CODE CLEANUP!!")
 class OrderBeerFragment : Fragment() {
 
     override fun onCreateView(
@@ -31,10 +29,88 @@ class OrderBeerFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         activity!!.nav_view.menu.findItem(R.id.nav_orderbeer).isChecked = true
         val token = context!!.getSharedPreferences(key, Context.MODE_PRIVATE)
-
         val json = JSONObject()
         json.put("action", "calcamount")
         json.put("payId", token.getString("payId", ""))
+        json.put("payId_from", token.getString("payId", ""))
+        btn_orderbeer.setOnClickListener {
+            if (txt_coins.text.toString() != "0") {
+                HttpTask {
+                    if (it == null) {
+                        println("connection error")
+                        AlertDialog.Builder(context)
+                            .setTitle("Fehler")
+                            .setMessage("Ups Bier verschüttet. Fehler können passieren. \n\n Fehlercode: " + HttpTask.msgError)
+                            .setPositiveButton("OK") { dialog, which ->
+                                SelectMenu(
+                                    -1,
+                                    drawer_layout,
+                                    activity
+                                ).change()
+                            }
+                            .show()
+
+                        return@HttpTask
+                    }
+                    val itJson = JsonParser().parse(it).asJsonObject
+                    if (itJson.get("result").asInt == 0) Toast.makeText(
+                        context!!,
+                        "Etwas ist schief gelaufen. War da ein Fuchs am Werk o_O",
+                        Toast.LENGTH_LONG
+                    ).show()
+                    else {
+                        var newCoins = itJson.get("amount").asInt - 1
+                        txt_coins.text = newCoins.toString()
+                        json.put("action", "newblock")
+                        json.put("amount", -1)
+                        HttpTask { it2 ->
+
+                            if (it2 == null) {
+                                println("connection error")
+                                AlertDialog.Builder(context)
+                                    .setTitle("Fehler")
+                                    .setMessage("Ups Bier verschüttet. Fehler können passieren. \n\n Fehlercode: " + HttpTask.msgError)
+                                    .setPositiveButton("OK") { dialog, which ->
+                                        SelectMenu(
+                                            -1,
+                                            drawer_layout,
+                                            activity
+                                        ).change()
+                                    }
+                                    .show()
+
+                                return@HttpTask
+                            }
+                            println(it2)
+                            val itJson2 = JsonParser().parse(it2).asJsonObject
+                            if (itJson2.get("result").asInt == 0) Toast.makeText(
+                                context!!,
+                                "Etwas ist schief gelaufen. War da ein Fuchs am Werk o_O",
+                                Toast.LENGTH_LONG
+                            ).show()
+                            else {
+                                Toast.makeText(
+                                    context!!,
+                                    "Bier erfolgreich bestellt",
+                                    Toast.LENGTH_LONG
+                                ).show()
+                                SelectMenu(R.id.nav_orderbeer, view, activity).change()
+                            }
+                        }.execute(
+                            "POST",
+                            "https://abidigital.tk/api/db_use.php",
+                            json.toString()
+                        )
+                    }
+                }.execute(
+                    "POST",
+                    "https://abidigital.tk/api/db_use.php",
+                    json.toString()
+                )
+            }
+        }
+
+
         HttpTask {
 
             if (it == null) {
